5d96d9d9810ed5f10c0b4eb1d1d305a543803d76
Ryansinghsohi <ryan.sohi84@gmail.com>
2025-09-18 11:53:52 +0200

    k

--- simple_calculator.js
+++ simple_calculator.js
@@ -1,6 +1,28 @@
-function calculateSum() {
+function calculate(operation) {
     let num1 = parseFloat(document.getElementById('num1').value);
     let num2 = parseFloat(document.getElementById('num2').value);
-    let sum = num1 + num2;
-    document.getElementById('result').innerHTML = 'Result: ' + sum;
-}
+    let result;
+
+    switch (operation) {
+        case 'add':
+            result = num1 + num2;   
+            break;
+        case 'subtract':
+            result = num1 - num2;   
+            break;      
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            if (num2 !== 0) {
+                result = num1 / num2;
+            } else {
+                result = 'Error: Division by zero';
+            }
+            break;
+        default:
+            result = 'Error: Unknown operation';
+    }
+
+    document.getElementById('result').innerHTML = 'Result: ' + result;
+}   
